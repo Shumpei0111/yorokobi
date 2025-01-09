dee95de6362312a95b7c6aebf2fd8025fe0721af
Shumpei <shumpei.o.7g@gmail.com>
2025-01-10 05:55:50 +0900

    fix(basic-auth): Basic 認証が動いていなかったので強制的に発火するように

--- src/env.mjs
+++ src/env.mjs
@@ -3,12 +3,15 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
+    /** ベーシック認証を有効にするかどうか */
+    ENABLE_BASIC_AUTH: z.string().transform((value) => value === "true"),
     /** ベーシック認証のユーザー名 */
     BASIC_AUTH_USER: z.string().min(1),
     /** ベーシック認証のパスワード */
     BASIC_AUTH_PASSWORD: z.string().min(1),
   },
   runtimeEnv: {
+    ENABLE_BASIC_AUTH: process.env.ENABLE_BASIC_AUTH,
     BASIC_AUTH_USER: process.env.BASIC_AUTH_USER,
     BASIC_AUTH_PASSWORD: process.env.BASIC_AUTH_PASSWORD,
   },
--- src/middleware.ts
+++ src/middleware.ts
@@ -11,7 +11,16 @@ const getNegotiatedLanguage = (
 
 export const config = {
   // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
-  matcher: ["/((?!api|_next/static|_next/image|.*\\.png$).*)"],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+  ],
 };
 
 export function middleware(request: NextRequest) {
@@ -20,12 +29,31 @@ export function middleware(request: NextRequest) {
     "basic-auth": request.headers.get("authorization") ?? "",
   };
 
-  if (headers["basic-auth"]) {
-    const basicAuth = headers["basic-auth"];
-    const authValue = basicAuth.split(" ")[1];
+  if (env.ENABLE_BASIC_AUTH) {
+    const authHeader = headers["basic-auth"];
+
+    if (!authHeader) {
+      return new Response("Unauthorized", {
+        status: 401,
+        headers: {
+          "WWW-Authenticate": "Basic realm=Authorization Required",
+          "Cache-Control": "no-store",
+          "Content-Type": "text/plain;charset=UTF-8",
+        },
+      });
+    }
+
+    const authValue = authHeader.split(" ")[1];
     const [user, password] = atob(authValue).split(":");
     if (user !== env.BASIC_AUTH_USER || password !== env.BASIC_AUTH_PASSWORD) {
-      return NextResponse.redirect(new URL("/", request.url));
+      return new Response("Unauthorized", {
+        status: 401,
+        headers: {
+          "WWW-Authenticate": "Basic realm=Authorization Required",
+          "Cache-Control": "no-store",
+          "Content-Type": "text/plain;charset=UTF-8",
+        },
+      });
     }
   }
 
@@ -36,6 +64,15 @@ export function middleware(request: NextRequest) {
     (lang) => !pathname.startsWith(`/${lang}/`) && pathname !== `/${lang}`
   );
 
+  /**
+   * MEMO: リダイレクトとリライトの処理
+   * --------------------------------------------------------------------------
+   * pathnameIsMissingLocale が true の場合、以下の処理が行われます：
+   * preferredLanguage が defaultLanguage と同じであれば、パスの先頭にデフォルトの言語をつけてリライトします。
+   * preferredLanguage が defaultLanguage と異なる場合は、ユーザーを preferredLanguage を含む新しいパスにリダイレクトします。
+   *
+   * この処理により、ユーザーがロケールを指定していない場合に、デフォルトの言語でページを表示するようにしています。
+   */
   if (pathnameIsMissingLocale) {
     if (preferredLanguage !== defaultLanguage) {
       return NextResponse.redirect(
