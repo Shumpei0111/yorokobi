081922bd5f1dfd655fa5e9dcfcbef98695c557e5
Shumpei <shumpei.o.7g@gmail.com>
2025-01-16 04:47:40 +0900

    style: 診断ボタンのスタイル調整

--- src/app/feature/taste-diagnosis/question-cards.tsx
+++ src/app/feature/taste-diagnosis/question-cards.tsx
@@ -1,6 +1,7 @@
 import { AnimatePresence, motion } from "motion/react";
 import { Question, UserAnswer } from "./types/questions";
 import { type Language } from "@/app/i18n/settings";
+import { tv } from "tailwind-variants";
 
 /** 質問カード */
 export const QuestionCards = ({
@@ -14,6 +15,29 @@ export const QuestionCards = ({
   lang: Language;
   handleAnswer: (answer: UserAnswer) => void;
 }) => {
+  const mainButtonVariant = tv({
+    base: "bg-[rgba(0,43,92,0.8)] hover:bg-[rgba(0,43,92,1)] duration-300 text-white py-2 px-4 rounded shadow font-jost border border-primary font-bold",
+    variants: {
+      lang: {
+        ja: "text-sm",
+        en: "text-base",
+      },
+    },
+  });
+
+  const subTextVariant = tv({
+    base: "text-xs block text-gray-100 pt-2 font-normal",
+    variants: {
+      lang: {
+        ja: "text-xs",
+        en: "text-sm",
+      },
+    },
+    defaultVariants: {
+      lang: "ja",
+    },
+  });
+
   return (
     <AnimatePresence>
       <div className="min-h-screen">
@@ -28,21 +52,24 @@ export const QuestionCards = ({
           animate={{ x: 0, opacity: 1 }}
           exit={{ x: -100, opacity: 0 }}
           transition={{ duration: 0.3 }}
-          className="absolute  -translate-x-1/2 top-1/3  w-full flex flex-col justify-center items-center  rounded p-6"
+          className="absolute  top-[20%]  w-full flex flex-col justify-center items-center  rounded p-6"
         >
           <h3 className="text-xl font-bold mb-4">
             {questions[currentIndex].question[lang]}
           </h3>
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col gap-4 w-full">
             {questions[currentIndex].options.map((option, index) => (
               <button
                 key={index}
-                className="bg-[rgba(0,43,92,0.8)] hover:bg-[rgba(0,43,92,1)] duration-300 text-white py-2 px-4 rounded shadow font-jost text-sm border border-primary"
+                className={mainButtonVariant({ lang })}
                 onClick={() => {
                   handleAnswer(option.scores);
                 }}
               >
                 {option.text[lang]}
+                <span className={subTextVariant({ lang })}>
+                  {option.subText[lang]}
+                </span>
               </button>
             ))}
           </div>
