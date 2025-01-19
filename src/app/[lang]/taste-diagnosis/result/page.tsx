2666cbd4a53a7f06c91d04ab6e7571d8c4b4eff9
Shumpei <shumpei.o.7g@gmail.com>
2025-01-20 04:55:05 +0900

    feat: 結果画面にi18n対応の仕込み

--- src/app/[lang]/taste-diagnosis/result/page.tsx
+++ src/app/[lang]/taste-diagnosis/result/page.tsx
@@ -1,3 +1,24 @@
-export default function ResultPage() {
+import { getTranslation } from "@/app/i18n/server";
+import { Language } from "@/app/i18n/settings";
+import { Metadata } from "next";
+
+export const generateMetadata = async ({
+  params,
+}: {
+  params: { lang: Language };
+}): Promise<Metadata> => {
+  const { t } = await getTranslation(params.lang);
+  return {
+    title: `Yorokobi | ${t("taste-diagnosis:おすすめ日本酒診断")}`,
+    description: t("taste-diagnosis:あなたにぴったりの日本酒を提案します"),
+  };
+};
+
+export default async function ResultPage({
+  params,
+}: {
+  params: { lang: Language };
+}) {
+  const { lang } = params;
   return <div>ResultPage</div>;
 }
