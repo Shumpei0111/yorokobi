6704a670c107a06c7a68447e9785522f0142bde3
Shumpei <shumpei.o.7g@gmail.com>
2025-01-21 08:01:34 +0900

    feat: 年齢確認用ページを設けてそこ経由にする

--- src/components/global/first-check-dialog.tsx
+++ src/components/global/first-check-dialog.tsx
@@ -9,8 +9,9 @@ import {
 import { Button } from "../ui/button";
 import { useTranslation } from "@/app/i18n/client";
 import { Language } from "@/app/i18n/settings";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SelectLang } from "../ui/select-lang";
+import { useRouter } from "next/navigation";
 
 export const FirstCheckDialog = ({ lang }: { lang: Language }) => {
   const { t } = useTranslation(lang);
@@ -18,11 +19,19 @@ export const FirstCheckDialog = ({ lang }: { lang: Language }) => {
     if (typeof window === "undefined") return false;
     return window.localStorage.getItem("ageCheck") === "true" ? false : true;
   });
+  const router = useRouter();
 
   const handleYes = () => {
     setOpen(false);
     window.localStorage.setItem("ageCheck", "true");
+    router.push(`/${lang}`);
   };
+
+  useEffect(() => {
+    if (open) {
+      router.push(`/${lang}/check`);
+    }
+  }, [open, lang, router]);
 
   return (
     <Dialog open={open} defaultOpen={open}>
@@ -40,11 +49,11 @@ export const FirstCheckDialog = ({ lang }: { lang: Language }) => {
               {t(
                 "common:あなたの国でアルコールを飲むことを認められた年齢ですか？"
               )}
-              <div className="flex gap-2 justify-center py-4">
-                <Button onClick={handleYes}>{t("common:はい")}</Button>
-                <Button>{t("common:いいえ")}</Button>
-              </div>
             </DialogDescription>
+            <div className="flex gap-2 justify-center py-4">
+              <Button onClick={handleYes}>{t("common:はい")}</Button>
+              <Button>{t("common:いいえ")}</Button>
+            </div>
           </div>
         </DialogHeader>
       </DialogContent>
