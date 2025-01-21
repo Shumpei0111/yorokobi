bb2511434c2214397cdc6647524cbde978ea6651
Shumpei <shumpei.o.7g@gmail.com>
2025-01-22 03:45:14 +0900

    feat: 診断データがない場合の画面を拡充

--- src/app/[lang]/taste-diagnosis/result/page.tsx
+++ src/app/[lang]/taste-diagnosis/result/page.tsx
@@ -2,7 +2,9 @@ import { getDiagnosisResult } from "@/app/feature/taste-diagnosis/actions/action
 import { DiagnosisResult } from "@/app/feature/taste-diagnosis/diagnosis-result";
 import { getTranslation } from "@/app/i18n/server";
 import { Language } from "@/app/i18n/settings";
+import { RotateCcw } from "lucide-react";
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const generateMetadata = async ({
   params,
@@ -22,9 +24,31 @@ export default async function ResultPage({
   params: { lang: Language };
 }) {
   const { lang } = params;
+  const { t } = await getTranslation(lang);
   const result = await getDiagnosisResult();
+
   if (!result) {
-    return <div>診断結果がありません</div>;
+    return (
+      <div className="flex justify-center items-center h-screen px-4">
+        <div className="text-center">
+          <h1 className="text-3xl font-bold">Oops!</h1>
+          <p className="text-gray-500 mt-2 text-sm">
+            {t(
+              "taste-diagnosis:診断データがありません。ぜひこちらから診断をやり直してください。"
+            )}
+          </p>
+          <div className="mt-4">
+            <Link
+              href={`/${lang}/taste-diagnosis`}
+              className="bg-[rgba(0,43,92,0.8)] hover:bg-[rgba(0,43,92,1)] hover:text-white duration-300 text-white py-2 px-10 rounded shadow font-jost border border-primary font-bold inline-flex items-center gap-2 text-sm"
+            >
+              <RotateCcw className="w-4 h-4" />
+              {t("taste-diagnosis:診断をやり直す")}
+            </Link>
+          </div>
+        </div>
+      </div>
+    );
   }
 
   const scores = {
